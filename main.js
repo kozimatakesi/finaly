7ff36c1f4e7978f3ee74de0b77baeb0c4e25adb9
riverbook708 <riverbook708@gmail.com>
2021-08-16 18:51:22 +0900

    Change time

--- main.js
+++ main.js
@@ -47,6 +47,18 @@ function getIndex(value, arr, prop) {
   return -1; // 値が存在しなかったとき
 }
 
+const COEFFICIENT = 24 * 60 * 60 * 1000; // 日数とミリ秒を変換する係数
+const DATES_OFFSET = 70 * 365 + 17 + 1 + 1; // 「1900/1/0」～「1970/1/1」 (日数)
+const MILLIS_DIFFERENCE = 9 * 60 * 60 * 1000; // UTCとJSTの時差 (ミリ秒)
+
+function convertSn2Ut(serialNumber) { // シリアル値→UNIX時間(ミリ秒)
+  return (serialNumber - DATES_OFFSET) * COEFFICIENT - MILLIS_DIFFERENCE;
+}
+
+function dateFromSn(serialNumber) { // シリアル値→Date→hh:mm:ss表記
+  return new Date(convertSn2Ut(serialNumber)).toLocaleTimeString('ja-JP', { timeZone: 'Asia/Tokyo' });
+}
+
 // Excelファイルの取り込み
 ipcMain.on('readExcelFile', (e, dirPath) => {
   (async () => {
@@ -57,7 +69,7 @@ ipcMain.on('readExcelFile', (e, dirPath) => {
     console.log(sheet1A1);
     const sheet1_json_all = xutil.sheet_to_json(sheet1);
     const serchStart = sheet1_json_all.filter((obj) => obj[sheet1A1] === 'ログ開始' || obj[sheet1A1] === 'ログ終了' || (/^\d{1,3}.+/).test(obj[sheet1A1]));
-    console.log(serchStart);
+    console.log(dateFromSn(serchStart[2].__EMPTY_1));
   })();
 });
 
